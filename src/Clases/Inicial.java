86c80f7f9646cb783bb28ff52d82eaabab197605
Rene Quisaguano <renequisaguano@gmail.com>
2014-10-13 14:12:49 -0500

    Configurando tema
    Creando el codigo para que inicie el tema independientemente del
    reproductor

--- src/Clases/Inicial.java
+++ src/Clases/Inicial.java
@@ -5,6 +5,11 @@
 package Clases;
 
 import Menu.Principal;
+import ReproductorMain.Main;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
@@ -16,6 +21,14 @@ public class Inicial {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+       
+        try {            
+            UIManager.setLookAndFeel("de.muntjak.tinylookandfeel.TinyLookAndFeel");
+            //TinyLookAndFeel.setCurrentTheme(TinyLookAndFeel.getCurrentTheme());
+        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
         Principal p=new Principal();
         p.setVisible(true);
         
